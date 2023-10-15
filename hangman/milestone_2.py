7fc491533ac679ccaeb15e44c20ed557fc836950
Lekshmi Sudhikumar <skumarlekshmi@gmail.com>
2023-10-15 13:17:44 +0100

    feat: added comments to the file

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,12 +1,17 @@
 import random
 
+# list of fruits
 word_list = ["watermelon", "lychee", "mango", "apple", "pomegranate"]
 print(word_list)
 
+# random word from the list
 word = random.choice(word_list)
 print(word)
 
+# asking the user for a guess
 guess = input("Enter a letter: ")
+
+# conditions to validate the "guess" input
 if len(guess) == 1 and guess.isalpha():
     print("Good guess!")
 else:
